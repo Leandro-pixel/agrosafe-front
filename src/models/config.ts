42695294f4799e11f6fc3877c5551a432d56125a
leandro soares <leandro20002004@hotmail.com>
2025-03-07 11:17:29 -0300

    feat: fixing images and logo

--- src/models/config.ts
+++ src/models/config.ts
@@ -7,9 +7,9 @@ export default class Config {
 	constructor (
     public primary = '',
     public secondary = '',
-    public logoUrl = 'src/assets/logo_brands.png',
+    public logoUrl = '/icons/logo_brands.png',
     public tenantName = 'BrandsCard',
-    public secondaryLogoUrl = 'src/assets/icone_brands.png',
+    public secondaryLogoUrl = 'icons/icone_brands.png',
     public SmsMessage = '',
     public paymentProcessors = [] as PaymentProcessor[],
     public recaptchaKey = ''
